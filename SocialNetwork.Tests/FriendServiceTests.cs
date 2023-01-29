bc33f17ca071d66d2cd0c2422aa6eaa5705cc71f
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-01-29 19:59:27 +0300

    Добавлен тест проверки исключения при вводе неправильного email

---
+++ SocialNetwork.Tests/FriendServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class FriendServiceTests
+    {
+        [Test]
+        public void AddFriendReturnException()
+        {
+            var friendService = new FriendService();
+
+            var friendAddData = new FriendData()
+            {
+                user_id = 10,
+                friend_email = "12gmail"
+            };
+
+            Assert.Throws<ArgumentNullException>(() => friendService.AddFriend(friendAddData));
+        }
+    }
+}
